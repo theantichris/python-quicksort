975133eaaa1eb7ab5ab7dbd5a6bbb4e24e1a0c5f
Christopher <chris@theantichris.com>
2020-01-07 14:41:10 -0500

    randomly picking pivot, swapping pivot and end

--- quicksort.py
+++ quicksort.py
@@ -1,10 +1,18 @@
+from random import randrange
+
 def quicksort(list, start, end):
     if start >= end:
         return list
 
+    pivot_idx = randrange(start, end)
+    pivot_element = list[pivot_idx]
+    list[end], list[pivot_idx] = pivot_element, list[end]
+
     print(list[start])
     start += 1
-    quicksort(list, start, end)
+    return quicksort(list, start, end)
 
-colors = ["blue", "red", "green", "purple", "orange"]
-quicksort(colors, 0, len(colors) - 1)
+my_list = [32, 22]
+print("BEFORE: ", my_list)
+sorted_list = quicksort(my_list, 0, len(my_list) - 1)
+print("AFTER: ", sorted_list)
